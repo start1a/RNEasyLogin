997dfc4532ab44c3d9c36709f28fa85569296be1
hglee <hglee.finshot@gmail.com>
2021-12-23 11:05:44 +0900

    4가지 타입 인증 형태로 컴포넌트 수정 및 생체 인증 컴포넌트 추가

--- App.js
+++ App.js
@@ -6,98 +6,80 @@
  * @flow strict-local
  */
 
- import React, { useState } from 'react';
- import {
-   Button,
-   SafeAreaView, StyleSheet,
-   NativeModules,
-   TextInput,
-   Text
- } from 'react-native';
- 
- const { KeyChainModule } = NativeModules;
- 
- const App = () => {
- 
-   const [text, setText] = useState("");
-   const [userName, setUserName] = useState("");
-   const [password, setPassword] = useState("");
- 
-   return (
-     <SafeAreaView>
-       <Text style={styles.text}>{text}</Text>
-       <TextInput
-         style={styles.textInput}
-         placeholder='userName'
-         value={userName}
-         onChangeText={setUserName}
-       />
-       <TextInput
-         style={styles.textInput}
-         placeholder='password'
-         value={password}
-         onChangeText={setPassword}
-       />
-       <Button
-         style={styles.button}
-         title='추가'
-         onPress={() => {
-          KeyChainModule.save(userName, password)
-          .then(res => {
-            console.log(`res : ${res}`);
-            setText("saved!")
-          })
-          .catch(err => {
-            console.log(`error: ${JSON.stringify(err)}`);
-          })
-         }}
-       />
-       <Button
-         style={styles.button}
-         title='조회'
-         onPress={() => {
-          KeyChainModule.load(userName)
-          .then(res => {
-            console.log(`res : ${JSON.stringify(res)}`);
-            setText(JSON.stringify(res));
-          })
-          .catch(err => {
-            console.log(`error: ${JSON.stringify(err)}`);
-          })
-         }}
-       />
-       <Button
-         style={styles.button}
-         title='삭제'
-         onPress={() => {
-           KeyChainModule.delete(userName)
-           .then(res => {
-             console.log(`res : ${res}`);
-             setText("deleted!");
-           })
-           .catch(err => {
-             console.log(`error: ${JSON.stringify(err)}`);
-           })
-         }}
-       />
-     </SafeAreaView>
-   );
- };
- 
- const styles = StyleSheet.create({
-   text: {
-     textAlign: "center",
-     padding: 10
-   },
-   textInput: {
-     paddingHorizontal: 8,
-     paddingVertical: 4
-   },
-   button: {
-     paddingVertical: 4,
-     paddingHorizontal: 16
-   }
- });
- 
- export default App;
- 
+import React, { useState } from 'react';
+import {
+  Button, FlatList, SafeAreaView, StyleSheet, Text, View
+} from 'react-native';
+import AuthBiometric from './src/Components/AuthBiometric';
+
+
+
+const App = () => {
+
+  const LOGINTYPES = [
+    "PINNUMBER",
+    "PATTERN",
+    "FINGERPRINT",
+    "FACE"
+  ]
+
+  const [typeLogin, setTypeLogin] = useState(LOGINTYPES[0]);
+
+  const AuthByType = () => {
+    switch (typeLogin) {
+      case LOGINTYPES[0]:
+        return <View />
+      case LOGINTYPES[1]:
+        return <View />
+      case LOGINTYPES[2]:
+      case LOGINTYPES[3]:
+        return <AuthBiometric />
+    }
+  }
+
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FlatList
+        style={{ flex: 4, alignSelf: 'center' }}
+        data={LOGINTYPES}
+        horizontal
+        renderItem={({ item }) => (
+          item !== typeLogin && <Button
+            title={item}
+            onPress={() => {
+              setTypeLogin(item);
+            }}
+          />
+        )}
+      />
+
+      <Text style={styles.text}>{typeLogin}</Text>
+
+      <View style={{ flex: 16 }}>
+          {AuthByType()}
+      </View>
+
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    textAlign: "center",
+    fontSize: 28,
+    fontWeight: 'bold',
+    padding: 10
+  },
+  textInput: {
+    paddingHorizontal: 8,
+    paddingVertical: 4
+  },
+  button: {
+    paddingVertical: 4,
+    paddingHorizontal: 16
+  }
+});
+
+export default App;
+
---
+++ src/Components/AuthBiometric.js
@@ -0,0 +1,172 @@
+import React, { useEffect, useState } from 'react';
+import {
+    View,
+    Text,
+    Button,
+    Alert,
+    Platform
+} from 'react-native';
+import FingerprintScanner from 'react-native-fingerprint-scanner';
+import { KeyChainModule, BiometricStateModule } from '../NativeModuleIndex';
+
+const AuthBiometric = () => {
+
+    // TouchId + FaceId
+    const BiometricScanner = FingerprintScanner;
+    const BiometricController = Platform.OS == 'android' ? BiometricController_Android : BiometricController_IOS;
+
+    // 로그인 정보 저장 키
+    const biometricKey = "login";
+
+    const [existBioId, setExistBioId] = useState(false);
+    const [isLogin, setIsLogin] = useState(false);
+
+    useEffect(() => {
+        checkBiometricStateChanged();
+        checkExistBiometricHistory();
+    }, [])
+
+
+    const checkExistBiometricHistory = () => {
+        BiometricController.checkExist(biometricKey)
+        .then(isExist => {
+            setExistBioId(isExist);
+        })
+        .catch(err => {
+            Alert.alert("CheckBiometricEnrolled", err.message);
+        })
+    }
+
+    const saveBiometricHistory = () => {
+        BiometricController.save(biometricKey)
+        .then(() => {
+            setExistBioId(true);
+        })
+        .catch(err => {
+            Alert.alert("SaveBiometricInfo", err.message);
+        })
+    }
+
+
+    const deleteBiometricHistory = async (onFinished = () => { }) => {
+        BiometricController.delete(biometricKey)
+        .then(() => {
+            setExistBioId(false);
+            setIsLogin(false);
+            onFinished();
+        })
+        .catch(err => {
+            Alert.alert("DeleteBiometricInfo", err.message);
+        })
+    }
+
+    const checkBiometricStateChanged = async () => {
+        try {
+            const isChanged = !(await BiometricController.checkChanged(biometricKey));
+            if (isChanged) {
+                deleteBiometricHistory(() => {
+                    Alert.alert("checkBiometricStateChanged", "Changes in biometric state is detected. Please proceed with biometric authentication Again.");
+                })
+            }
+            return isChanged;
+        } catch (err) {
+            Alert.alert("CheckBiometricState", err.message);
+            return true;
+        }
+    }
+
+
+    const authByBiometric = (onSucceed = () => { }) => {
+        BiometricScanner.authenticate({ description: "Login With Authentication" })
+            .then(() => {
+                onSucceed();
+            })
+            .catch(err => {
+                Alert.alert(err.name, err.message);
+            })
+    }
+
+    return (
+        <View style={{ flex: 1, paddingVertical: 10 }}>
+            {
+                isLogin && <Button
+                    style={{ padding: 16 }}
+                    title='로그아웃'
+                    onPress={() => { setIsLogin(false); }} />
+            }
+
+            {
+                !isLogin && (existBioId ?
+                    <Button
+                        style={{ padding: 16 }}
+                        title='로그인'
+                        onPress={async () => {
+                            const isChanged = await checkBiometricStateChanged();
+                            if (!isChanged) authByBiometric(() => { setIsLogin(true); });
+                        }} />
+                    : <Button
+                        style={{ padding: 16 }}
+                        title='등록'
+                        onPress={() => {
+                            authByBiometric(() => {
+                                saveBiometricHistory();
+                            });
+                        }}
+                    />)
+            }
+
+            {isLogin && existBioId ?
+                <Button
+                    style={{ padding: 16 }}
+                    title='삭제'
+                    onPress={() => {
+                        deleteBiometricHistory();
+                    }}
+                /> : null}
+        </View>
+    )
+}
+
+
+const BiometricController_Android = class {
+    static checkExist = (key) => {
+
+    }
+
+    static checkChanged111 = (key) => {
+
+    }
+
+    static save = (key) => {
+
+    }
+
+    static delete = (key) => {
+
+    }
+}
+
+const BiometricController_IOS = class {
+    static checkExist = (key) =>
+        BiometricStateModule.isExist(key);
+
+    static checkChanged = (key) =>
+        BiometricStateModule.check(key)
+
+    static save = (key) =>
+        BiometricStateModule.save(key)
+            .then(() => {
+                return KeyChainModule.save(key, JSON.stringify({
+                    userName: "abcdefg",
+                    password: "a123456"
+                }));
+            })
+
+    static delete = (key) =>
+        BiometricStateModule.delete(key)
+            .then(() => {
+                return KeyChainModule.delete(key);
+            })
+}
+
+export default AuthBiometric;
